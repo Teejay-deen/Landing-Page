6688b991541eed94c441c6b020784e50d1c5fa05
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-09-26 18:21:47 +0100

    sticky navbar and scrolling animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 module.exports = {
     theme: {
       extend: {
-        fontFamily: {
-          clash: ['"Clash Display"', 'sans-serif'],
-          dmSans: ['"DM Sans"', 'sans-serif'],
-          cabin: ["Cabin", "sans-serif"],
-          
+        animation: {
+            "loop-scroll": "loop-scroll 50s linear inifinite"
         },
+
+        keyframes: {
+            "loop-scroll":{
+                from: {transform: "translateX(0)"},
+                to: {transform: "translateX(-100%)"},
+            }
+        },
+        // fontFamily: {
+        //   clash: ['"Clash Display"', 'sans-serif'],
+        //   dmSans: ['"DM Sans"', 'sans-serif'],
+        //   cabin: ["Cabin", "sans-serif"],
+          
+        // },
       },
     },
     plugins: [],
