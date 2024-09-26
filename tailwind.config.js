567c70c36f2235b35442a214d463b64e32016c6f
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-09-26 12:06:08 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          clash: ['"Clash Display"', 'sans-serif'],
+          dmSans: ['"DM Sans"', 'sans-serif'],
+          cabin: ["Cabin", "sans-serif"],
+          
+        },
+      },
+    },
+    plugins: [],
+  }
+  
