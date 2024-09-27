ae66229bdbbfebc3d59305dcc2b83d43f61bd812
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-09-27 12:10:34 +0100

    refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,37 @@
-module.exports = {
+// module.exports = {
+//     theme: {
+//       extend: {
+//         animation: {
+//             "loop-scroll": "loop-scroll 50s linear inifinite"
+//         },
+
+//         keyframes: {
+//             "loop-scroll":{
+//                 from: {transform: "translateX(0)"},
+//                 to: {transform: "translateX(-100%)"},
+//             }
+//         },
+        
+//       },
+//     },
+//     plugins: [],
+//   }
+
+
+  module.exports = {
     theme: {
       extend: {
-        animation: {
-            "loop-scroll": "loop-scroll 50s linear inifinite"
+        backgroundImage: {
+          'custom-svg': "url('/path-to-your-image.svg')",
         },
 
-        keyframes: {
-            "loop-scroll":{
-                from: {transform: "translateX(0)"},
-                to: {transform: "translateX(-100%)"},
-            }
-        },
-        // fontFamily: {
-        //   clash: ['"Clash Display"', 'sans-serif'],
-        //   dmSans: ['"DM Sans"', 'sans-serif'],
-        //   cabin: ["Cabin", "sans-serif"],
-          
-        // },
+        fontFamily: {
+            clash: ['"Clash Display"', 'sans-serif'],
+            dmSans: ['"DM Sans"', 'sans-serif'],
+            cabin: ["Cabin", "sans-serif"],
+            
+          },
       },
     },
-    plugins: [],
   }
   
